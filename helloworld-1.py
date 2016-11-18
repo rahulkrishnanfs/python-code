ad6055275a191557d3adef70d114cde0ea648dec
Rahulkrishnan R A <rahulkrishnanfs@gmail.com>
2016-11-19 01:51:36 +0530

    Hellow world

---
+++ helloworld-1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# Usage of string literals
+
+print "Hello world"
+print 'Hellow world'
+print "Rahul's world of python"
+print "'I' am a good programmer"
+print '"I" will start my career as DevOps' 
