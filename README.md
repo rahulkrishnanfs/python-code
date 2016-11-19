a090626530bc84b6b84f7cd8c5eee90637bad7d9
Rahulkrishnan R A <rahulkrishnanfs@gmail.com>
2016-11-19 10:26:35 +0530

    Variable changed

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Important Tips and links to follow
 1) Integration of pylint with vim editor
 
 http://www.vim.org/scripts/script.php?script_id=891 
+
+2) Setup Virtual Environment
+
+apt-get ge
--- Variable.py
+++ Variable.py
@@ -3,9 +3,11 @@
 car_available = 100
 drivers_present = 20
 passengers_travelled = 120
-car_space = 12
+car_space = 13
 car_pool_capacity = drivers_present * car_space
 print "Available car is ", drivers_present
 print "car pool capacity is", car_pool_capacity
 print "Car not driven ", car_available - drivers_present
 
+# test for space between the string
+print "Hello I am a programmer"
